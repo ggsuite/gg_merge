65a1fd79544685f8aa7d8e50138e7210f3d28914
Göran Hegenberg <goeran@hegenberg-privat.de>
2026-09-11 09:33:52 +0200

    #gg: Finish development of version 3.7.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 3.7.0 - 2026-09-11
 
 ### Fixed
 
--- lib/src/gg_merge_version.dart
+++ lib/src/gg_merge_version.dart
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `gg_merge` package.
-const String ggMergeVersion = '3.6.0';
+const String ggMergeVersion = '3.7.0';
